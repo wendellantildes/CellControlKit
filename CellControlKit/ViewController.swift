1df35da9829ec522fac01cdc0ead1e1cbf02470b
Wendell Antildes <wendellantildes@gmail.com>
2017-02-12 17:30:24 -0300

    Initial Commit

---
+++ CellControlKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CellControlKit
+//
+//  Created by Wendell Antildes M Sampaio on 12/02/2017.
+//  Copyright © 2017 Wendell Antildes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
